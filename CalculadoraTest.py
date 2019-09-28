64f3186273ab957ea8b67791b3103266abb5034e
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-27 21:01:18 -0500

    create CalculadoraTest

---
+++ CalculadoraTest.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+import Calculadora
+
+class CalculadoraTest(TestCase):
+    def test_sumar(self):
+        self.assertEqual(Calculadora.Calculadora.sumar(""),0,"Cadena vacia")
